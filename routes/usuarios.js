50e2f27cc07ee0f61805d6e089e7ee26cd800dd1
AnaGFranco <anagsfranco@hotmail.com>
2020-03-10 22:47:54 -0300

    add routes

---
+++ routes/usuarios.js
@@ -0,0 +1,20 @@
+﻿const express = require('express');
+
+const router = express.Router();
+const controller = require("../controllers/UsuariosController")
+
+router.get('', controller.getAll) // ler todos usuário
+router.post('', controller.add) // add usuário
+router.get('/:id', controller.getById) // ler usuário por id
+router.patch('/:id', controller.update) // atualizar usuário por id
+router.delete('/:id',  controller.remove) // deletar usuário por id
+router.post('/:usuarioId/telefones', controller.addTelefone)  // add usuário
+router.get('/:usuarioId/telefones',  controller.getTelefones) // ler telefones por usuario
+router.patch('/::usuarioId/telefones/:telefoneId', controller.updateTelefone) //atualizar telefone
+router.delete('/:usuarioId/telefones/:telefoneId', controller.removeTelefone) //deletar telefone
+router.post('/login', controller.login) // realizar login
+
+module.exports = router
+
+
+
