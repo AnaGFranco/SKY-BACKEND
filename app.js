56115acd58a73d6a0e06b267debb92588dfa376e
AnaGFranco <anagsfranco@hotmail.com>
2020-03-10 22:40:34 -0300

    instalando dependencias e estruturando app

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = express()
+const usuarios = require('./routes/usuarios')
+const PORT = 3000
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/usuarios', usuarios)
+
+app.get('/', (request, response) => {
+  response.send('Olá, mundo!')
+})
+
+app.listen(PORT)
+console.info(`Rodando na porta ${PORT}`)
