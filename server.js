d15e0418487a7526f6aec2bb9851e13e7335adc1
AnaGFranco <anagsfranco@hotmail.com>
2020-03-13 13:42:04 -0300

    alteracao de porta

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./src/app")
-const Port = process.env.Port || 5000
+const Port = process.env.Port || 3000
 
 app.listen(Port, () => {
     console.log(`Servidor está rodando na Porta:${Port}`)
